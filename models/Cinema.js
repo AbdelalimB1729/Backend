2217b8037f022ea5f85fb5141c27731ade09dead
Abdelalim <abdelalim.study@gmail.com>
2025-07-20 18:35:33 -0400

    .

--- models/Cinema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const cinemaSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  location: String,
-  numberOfSeats: Number
-});
-
-module.exports = mongoose.model('Cinema', cinemaSchema);
